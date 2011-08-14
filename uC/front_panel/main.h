ef90aeac803319bb2c507b827d5a3b85273b1068
mike@sm3wmv.com <mike@sm3wmv.com@bdcc0732-df4f-11de-b4e8-61bb9a648e99>
2011-08-14 19:30:23 +0000

    - Fixed so that it's not firmware ver 0.4.5

--- uC/front_panel/main.h
+++ uC/front_panel/main.h
@@ -26,7 +26,7 @@
 //#define DEBUG_COMPUTER_USART_ENABLED 1
 
 //! The current firmware revision nr
-#define FIRMWARE_REV "0.42b\0"
+#define FIRMWARE_REV "0.4.5\0"
 
 //! Macro to enable timer 0 interrupt
 #define ENABLE_TIMER0_INT() 	TIMSK0 |= (1<<OCIE0A);
