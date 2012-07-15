740d77cb682bb34254358cca86d589531a07a992
sm0svx <sm0svx@bdcc0732-df4f-11de-b4e8-61bb9a648e99>
2012-07-15 09:16:16 +0000

    front_panel: - Bumped the firmware revision number one step.

--- uC/front_panel/main.h
+++ uC/front_panel/main.h
@@ -26,7 +26,7 @@
 //#define DEBUG_COMPUTER_USART_ENABLED 1
 
 //! The current firmware revision nr
-#define FIRMWARE_REV "308\0"
+#define FIRMWARE_REV "309\0"
 
 //! Macro to enable timer 0 interrupt
 #define ENABLE_TIMER0_INT() 	TIMSK0 |= (1<<OCIE0A);
