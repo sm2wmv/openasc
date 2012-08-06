f2e10d11df32d2900b62843dde401f218718b8f4
mike@sm3wmv.com <mike@sm3wmv.com@bdcc0732-df4f-11de-b4e8-61bb9a648e99>
2012-08-06 08:58:03 +0000

    - Various work on support op

--- trunk/uC/support_op/ext_control.h
+++ trunk/uC/support_op/ext_control.h
@@ -2,11 +2,35 @@
 #define _EXT_CONTROL_H_
 
 enum led_state {
-  LED_STATE_ON,
-  LED_STATE_OFF
+  LED_ON,
+  LED_OFF
+};
+
+enum relay_state {
+  RELAY_ON,
+  RELAY_OFF
+};
+
+enum ptt_state {
+  PTT_ACTIVE,
+  PTT_DEACTIVE
 };
 
 void ext_control_set_led_yellow(enum led_state state);
 void ext_control_set_led_red(enum led_state state);
+
+void ext_control_set_relay_k4(enum relay_state state);
+void ext_control_set_relay_k5k6(enum relay_state state);
+void ext_control_set_relay_k8(enum relay_state state);
+void ext_control_set_relay_k9(enum relay_state state);
+void ext_control_set_relay_k10(enum relay_state state);
+void ext_control_set_relay_k11(enum relay_state state);
+void ext_control_set_relay_k13(enum relay_state state);
+
+void ext_control_r1_ptt_radio(enum ptt_state state);
+void ext_control_r2_ptt_radio(enum ptt_state state);
+
+void ext_control_r1_ptt_amp(enum ptt_state state);
+void ext_control_r2_ptt_amp(enum ptt_state state);
 
 #endif
